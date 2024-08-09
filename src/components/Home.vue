3b9efe52b01f56b7496546fbc61294085c1ea376
rodrigobecher <rodrigofbecher@gmail.com>
2024-08-08 21:13:37 -0300

    novidades

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,105 @@
-<script setup>
+<script>
+export default {
+
+    data() {
+        return {
+            position: null,
+            error: null,
+        }
+    },
+
+    mounted() {
+        this.getLocation();
+        //this.getDadosChuva();
+    },
+
+    methods: {
+
+        getDadosChuva() {
+            const token = '64da91beeaa1c30d062783decb2d6e25'
+            const latitude = this.position.coords.latitude
+            const longitude = this.position.coords.longitude
+            const resource = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${token}`;
+            this.axios.get(resource).then((response) => {
+                console.log(response.data)
+        })
+        },
+
+        getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+            } else { 
+                console.log("Geolocation is not supported by this browser.");
+            }
+        },
+        showPosition(position) {
+            this.position = position;
+            //this.getDadosChuva();
+        },
+
+        showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    this.error = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    this.error = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    this.error = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    this.error = "An unknown error occurred."
+                    break;
+            }
+        }
+
+    }
+}
 </script>
 
 <template>
+    <div class="card w-50">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm">
+                    <h5 class="card-title">Previsão do tempo</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    <img class="wob_tci" alt="Pancadas leves de chuva" src="//ssl.gstatic.com/onebox/weather/64/rain_light.png" id="wob_tci" data-csiid="KU61ZuzQL87N1sQP_t_4-AQ_13" data-atf="1">
+                    <span style="font-weight: bold;font-size: 30px;">19</span><span >&deg;C</span>
+                </div>
+                <div class="col-sm" style="text-align: end;">
+                    <div style="font-weight: bold;">
+                        {{ 'Clima' }}
+                    </div>
+                    <div>
+                        {{'Quinta-feira'}}
+                    </div>
+                    <div>
+                        {{'Pancadas de chuva'}}   
+                    </div>
+                </div>  
+            </div>
+            <div class="row">
+                <div class="col-sm">
+                    {{ 'Hoje não é um bom dia para sair com seu velinho' }}
+                </div>
+            </div>
+        </div>
+    </div> 
 </template>
+<style scoped>
+.card {
+    float: right;
+    margin: 10px;
+    border: unset;
+}
+
+.celcius {
+    position: absolute;
+    bottom: 53px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import VueSidebarMenu from 'vue-sidebar-menu'
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import "vue-sidebar-menu/src/scss/vue-sidebar-menu.scss";
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
+app.use(VueAxios, axios)
 app.use(router)
 app.use(VueSidebarMenu)
 
